5e7d046ebb8a0199f7127cb384dd6c92b994ed9c
Juan León <juans.leonv@icloud.com>
2024-04-01 20:19:09 -0500

    feat: page 404 done

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,7 +1,9 @@
-export const ErrorPage = () => {
-  return (
-    <>
-      <h1>Error page</h1>
-    </>
-  );
-};
+import { Error } from "./sections/Error";
+
+export const ErrorPage = () => (
+  <Error
+    error="404"
+    message="Página no encontrada"
+    contextMessage="¡Oh, disculpa el desvío celestial! Parece que has tomado una ruta interdimensional que no conduce a ningún destino conocido. Los ángeles guardianes digitales están desplegando sus alas para guiarte de nuevo a la senda correcta."
+  />
+);
---
+++ src/sections/Error.tsx
@@ -0,0 +1,44 @@
+import { Action } from "../components/Action";
+import { Typography } from "../components/Typography";
+import Logo from "../icons/CamilaGacha.svg";
+
+type ErrorProps = {
+  error: string;
+  message: string;
+  contextMessage: string;
+};
+
+export const Error = ({ error, message, contextMessage }: ErrorProps) => (
+  <section className="m-auto flex w-full flex-wrap place-items-center items-center justify-center">
+    <a className="w-96" href="/">
+      <img src={Logo} alt="Camila Gacha Ángeles" />
+    </a>
+    <div className="m-5 md:m-10 text-center">
+      <Typography
+        as="h1"
+        variant="title"
+        color="marron"
+        customClasses="mb-10"
+        content={`error ${error}`}
+      />
+      <Typography
+        as="h2"
+        variant="subtitle"
+        color="marron"
+        content={`<p>${message}</p>`}
+      />
+
+      <p className="font-sans my-5 max-w-[410px] text-body-sm md:text-body-md">
+        {contextMessage}
+      </p>
+
+      <Action
+        color="marron"
+        as="a"
+        aria-label="Volver a la página principal"
+        href="/"
+        content="Ir al inicio"
+      />
+    </div>
+  </section>
+);
