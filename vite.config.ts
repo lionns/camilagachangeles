e49c3e7b90c30e5dcceb4f8ad82c3486715ad875
Juan León <juans.leonv@icloud.com>
2024-04-04 09:21:19 -0500

    !feat: vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react", "react-router", "react-router-dom"],
+      external: ["react", "react-dom", "react-router-dom"],
     },
   },
 });
